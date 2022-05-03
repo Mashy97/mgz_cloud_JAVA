b259f3c7441a220d0b8e4f0ce2921b8e7d8d9f9a
Mashy97 <104823140+Mashy97@users.noreply.github.com>
2022-05-03 17:14:33 +0900

    Delete randomApp_0425 directory

--- randomApp_0425/src/RandomTest.java
+++
@@ -1,9 +0,0 @@
-
-public class RandomTest {
-	public static void main(String[] args) {
-		for (int i = 1; i<=100; i++) {
-			double ran = Math.random();
-			System.out.printf("ran : %f%n",ran);
-		}
-	}
-}
--- randomApp_0425/src/RandomTest02.java
+++
@@ -1,9 +0,0 @@
-
-public class RandomTest02 {
-	public static void main(String[] args) {
-		for(int i = 1; i<=10; i++) {
-			int ran = (int)(Math.random()*41+5); // 5 ~ 45 사이의 정수형 난수
-			System.out.println(ran);
-		}
-	}
-}
